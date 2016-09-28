405c6a1a6df9ab93dd80ae9f1d8f5dbb5a9e03bd
andribo <bohdan29@ukr.net>
2016-09-28 11:34:46 +0300

    added homework into JS Intro

---
+++ 4-Intro-to-JS/homework/people.js
@@ -0,0 +1,701 @@
+var people = [{
+  "firstName": "Jack",
+  "lastName": "Jackson",
+  "gender": "Male",
+  "favoriteColor": "Purple",
+  "country": "Indonesia",
+  "birthday": "1/11/1982"
+}, {
+  "firstName": "Howard",
+  "lastName": "Burke",
+  "gender": "Male",
+  "favoriteColor": "Violet",
+  "country": "Poland",
+  "birthday": "4/8/1984"
+}, {
+  "firstName": "Donna",
+  "lastName": "Foster",
+  "gender": "Female",
+  "favoriteColor": "Pink",
+  "country": "China",
+  "birthday": "1/2/1984"
+}, {
+  "firstName": "Marilyn",
+  "lastName": "Allen",
+  "gender": "Female",
+  "favoriteColor": "Goldenrod",
+  "country": "Czech Republic",
+  "birthday": "9/1/1993"
+}, {
+  "firstName": "Jonathan",
+  "lastName": "King",
+  "gender": "Male",
+  "favoriteColor": "Purple",
+  "country": "China",
+  "birthday": "7/18/1986"
+}, {
+  "firstName": "Rachel",
+  "lastName": "Lewis",
+  "gender": "Female",
+  "favoriteColor": "Turquoise",
+  "country": "China",
+  "birthday": "5/12/1993"
+}, {
+  "firstName": "Virginia",
+  "lastName": "Lee",
+  "gender": "Female",
+  "favoriteColor": "Indigo",
+  "country": "Brazil",
+  "birthday": "3/16/1983"
+}, {
+  "firstName": "Kathy",
+  "lastName": "Lane",
+  "gender": "Female",
+  "favoriteColor": "Fuscia",
+  "country": "Sweden",
+  "birthday": "11/29/1986"
+}, {
+  "firstName": "Gregory",
+  "lastName": "Ruiz",
+  "gender": "Male",
+  "favoriteColor": "Pink",
+  "country": "Indonesia",
+  "birthday": "8/5/1981"
+}, {
+  "firstName": "Nicholas",
+  "lastName": "Griffin",
+  "gender": "Male",
+  "favoriteColor": "Purple",
+  "country": "Portugal",
+  "birthday": "4/2/1983"
+}, {
+  "firstName": "Steven",
+  "lastName": "Armstrong",
+  "gender": "Male",
+  "favoriteColor": "Blue",
+  "country": "Syria",
+  "birthday": "3/1/1983"
+}, {
+  "firstName": "Todd",
+  "lastName": "Perkins",
+  "gender": "Male",
+  "favoriteColor": "Purple",
+  "country": "Russia",
+  "birthday": "9/6/1993"
+}, {
+  "firstName": "Jeremy",
+  "lastName": "Howard",
+  "gender": "Male",
+  "favoriteColor": "Violet",
+  "country": "Kazakhstan",
+  "birthday": "3/31/1995"
+}, {
+  "firstName": "Jeremy",
+  "lastName": "Evans",
+  "gender": "Male",
+  "favoriteColor": "Fuscia",
+  "country": "China",
+  "birthday": "6/27/1993"
+}, {
+  "firstName": "Cheryl",
+  "lastName": "Ruiz",
+  "gender": "Female",
+  "favoriteColor": "Green",
+  "country": "Peru",
+  "birthday": "5/30/1998"
+}, {
+  "firstName": "Kenneth",
+  "lastName": "Harvey",
+  "gender": "Male",
+  "favoriteColor": "Fuscia",
+  "country": "France",
+  "birthday": "3/20/1997"
+}, {
+  "firstName": "Sandra",
+  "lastName": "Ramos",
+  "gender": "Female",
+  "favoriteColor": "Green",
+  "country": "Colombia",
+  "birthday": "7/29/1981"
+}, {
+  "firstName": "Alan",
+  "lastName": "Cox",
+  "gender": "Male",
+  "favoriteColor": "Mauv",
+  "country": "China",
+  "birthday": "2/1/1987"
+}, {
+  "firstName": "Jeremy",
+  "lastName": "Ray",
+  "gender": "Male",
+  "favoriteColor": "Teal",
+  "country": "Japan",
+  "birthday": "1/24/1997"
+}, {
+  "firstName": "Carl",
+  "lastName": "Burke",
+  "gender": "Male",
+  "favoriteColor": "Fuscia",
+  "country": "China",
+  "birthday": "11/18/1984"
+}, {
+  "firstName": "Christine",
+  "lastName": "Jones",
+  "gender": "Female",
+  "favoriteColor": "Green",
+  "country": "Albania",
+  "birthday": "8/22/2000"
+}, {
+  "firstName": "Charles",
+  "lastName": "Collins",
+  "gender": "Male",
+  "favoriteColor": "Crimson",
+  "country": "Russia",
+  "birthday": "2/26/1992"
+}, {
+  "firstName": "Carlos",
+  "lastName": "Butler",
+  "gender": "Male",
+  "favoriteColor": "Indigo",
+  "country": "Iran",
+  "birthday": "10/23/1996"
+}, {
+  "firstName": "Julie",
+  "lastName": "Frazier",
+  "gender": "Female",
+  "favoriteColor": "Crimson",
+  "country": "Peru",
+  "birthday": "7/7/1991"
+}, {
+  "firstName": "Jane",
+  "lastName": "Warren",
+  "gender": "Female",
+  "favoriteColor": "Purple",
+  "country": "Jamaica",
+  "birthday": "4/25/1993"
+}, {
+  "firstName": "Shirley",
+  "lastName": "Chavez",
+  "gender": "Female",
+  "favoriteColor": "Teal",
+  "country": "Russia",
+  "birthday": "11/4/1991"
+}, {
+  "firstName": "Ryan",
+  "lastName": "Bell",
+  "gender": "Male",
+  "favoriteColor": "Yellow",
+  "country": "China",
+  "birthday": "5/14/1989"
+}, {
+  "firstName": "Nicole",
+  "lastName": "Collins",
+  "gender": "Female",
+  "favoriteColor": "Crimson",
+  "country": "China",
+  "birthday": "6/5/2000"
+}, {
+  "firstName": "Stephanie",
+  "lastName": "Mitchell",
+  "gender": "Female",
+  "favoriteColor": "Fuscia",
+  "country": "North Korea",
+  "birthday": "8/27/1984"
+}, {
+  "firstName": "Sandra",
+  "lastName": "Kim",
+  "gender": "Female",
+  "favoriteColor": "Khaki",
+  "country": "China",
+  "birthday": "5/23/1998"
+}, {
+  "firstName": "Alan",
+  "lastName": "Barnes",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "Indonesia",
+  "birthday": "11/1/1980"
+}, {
+  "firstName": "Rose",
+  "lastName": "Gomez",
+  "gender": "Female",
+  "favoriteColor": "Red",
+  "country": "Venezuela",
+  "birthday": "1/1/1992"
+}, {
+  "firstName": "Tina",
+  "lastName": "Montgomery",
+  "gender": "Female",
+  "favoriteColor": "Violet",
+  "country": "China",
+  "birthday": "6/11/1997"
+}, {
+  "firstName": "Jack",
+  "lastName": "Little",
+  "gender": "Male",
+  "favoriteColor": "Fuscia",
+  "country": "Uganda",
+  "birthday": "7/15/1993"
+}, {
+  "firstName": "Debra",
+  "lastName": "Lewis",
+  "gender": "Female",
+  "favoriteColor": "Teal",
+  "country": "China",
+  "birthday": "9/15/1993"
+}, {
+  "firstName": "Elizabeth",
+  "lastName": "Hamilton",
+  "gender": "Female",
+  "favoriteColor": "Blue",
+  "country": "Colombia",
+  "birthday": "8/14/1985"
+}, {
+  "firstName": "Catherine",
+  "lastName": "Morales",
+  "gender": "Female",
+  "favoriteColor": "Yellow",
+  "country": "Indonesia",
+  "birthday": "10/19/1984"
+}, {
+  "firstName": "Larry",
+  "lastName": "Greene",
+  "gender": "Male",
+  "favoriteColor": "Turquoise",
+  "country": "Czech Republic",
+  "birthday": "9/23/1986"
+}, {
+  "firstName": "George",
+  "lastName": "Sims",
+  "gender": "Male",
+  "favoriteColor": "Blue",
+  "country": "China",
+  "birthday": "2/12/1991"
+}, {
+  "firstName": "Jerry",
+  "lastName": "Edwards",
+  "gender": "Male",
+  "favoriteColor": "Goldenrod",
+  "country": "Indonesia",
+  "birthday": "9/18/1991"
+}, {
+  "firstName": "Benjamin",
+  "lastName": "Larson",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "Malaysia",
+  "birthday": "2/7/1987"
+}, {
+  "firstName": "Lillian",
+  "lastName": "Stone",
+  "gender": "Female",
+  "favoriteColor": "Puce",
+  "country": "Libya",
+  "birthday": "7/20/1990"
+}, {
+  "firstName": "Jose",
+  "lastName": "Ford",
+  "gender": "Male",
+  "favoriteColor": "Blue",
+  "country": "Indonesia",
+  "birthday": "12/22/1990"
+}, {
+  "firstName": "Laura",
+  "lastName": "Dunn",
+  "gender": "Female",
+  "favoriteColor": "Maroon",
+  "country": "China",
+  "birthday": "9/18/1985"
+}, {
+  "firstName": "Doris",
+  "lastName": "West",
+  "gender": "Female",
+  "favoriteColor": "Indigo",
+  "country": "Philippines",
+  "birthday": "8/6/1991"
+}, {
+  "firstName": "Sara",
+  "lastName": "Ray",
+  "gender": "Female",
+  "favoriteColor": "Indigo",
+  "country": "Russia",
+  "birthday": "10/5/1981"
+}, {
+  "firstName": "Sean",
+  "lastName": "Reyes",
+  "gender": "Male",
+  "favoriteColor": "Yellow",
+  "country": "China",
+  "birthday": "1/31/1995"
+}, {
+  "firstName": "Larry",
+  "lastName": "Peters",
+  "gender": "Male",
+  "favoriteColor": "Indigo",
+  "country": "Eritrea",
+  "birthday": "4/11/1995"
+}, {
+  "firstName": "Earl",
+  "lastName": "Holmes",
+  "gender": "Male",
+  "favoriteColor": "Maroon",
+  "country": "Indonesia",
+  "birthday": "10/2/1997"
+}, {
+  "firstName": "Lisa",
+  "lastName": "Baker",
+  "gender": "Female",
+  "favoriteColor": "Pink",
+  "country": "France",
+  "birthday": "7/19/1985"
+}, {
+  "firstName": "Sandra",
+  "lastName": "Watson",
+  "gender": "Female",
+  "favoriteColor": "Indigo",
+  "country": "China",
+  "birthday": "8/21/1985"
+}, {
+  "firstName": "Jeffrey",
+  "lastName": "Ramirez",
+  "gender": "Male",
+  "favoriteColor": "Turquoise",
+  "country": "Indonesia",
+  "birthday": "2/12/1990"
+}, {
+  "firstName": "Jimmy",
+  "lastName": "Reed",
+  "gender": "Male",
+  "favoriteColor": "Khaki",
+  "country": "China",
+  "birthday": "10/2/1983"
+}, {
+  "firstName": "Karen",
+  "lastName": "Robinson",
+  "gender": "Female",
+  "favoriteColor": "Violet",
+  "country": "Canada",
+  "birthday": "8/3/2000"
+}, {
+  "firstName": "Rebecca",
+  "lastName": "Woods",
+  "gender": "Female",
+  "favoriteColor": "Teal",
+  "country": "Philippines",
+  "birthday": "11/2/1997"
+}, {
+  "firstName": "Lois",
+  "lastName": "James",
+  "gender": "Female",
+  "favoriteColor": "Puce",
+  "country": "Japan",
+  "birthday": "4/10/1989"
+}, {
+  "firstName": "Emily",
+  "lastName": "Smith",
+  "gender": "Female",
+  "favoriteColor": "Indigo",
+  "country": "Portugal",
+  "birthday": "6/23/1982"
+}, {
+  "firstName": "Ralph",
+  "lastName": "Alexander",
+  "gender": "Male",
+  "favoriteColor": "Turquoise",
+  "country": "Uruguay",
+  "birthday": "9/3/1984"
+}, {
+  "firstName": "Anthony",
+  "lastName": "Thompson",
+  "gender": "Male",
+  "favoriteColor": "Green",
+  "country": "France",
+  "birthday": "2/8/2000"
+}, {
+  "firstName": "Carl",
+  "lastName": "Morgan",
+  "gender": "Male",
+  "favoriteColor": "Yellow",
+  "country": "Ecuador",
+  "birthday": "11/24/1993"
+}, {
+  "firstName": "Harry",
+  "lastName": "Boyd",
+  "gender": "Male",
+  "favoriteColor": "Orange",
+  "country": "Philippines",
+  "birthday": "10/17/1986"
+}, {
+  "firstName": "Wanda",
+  "lastName": "Morales",
+  "gender": "Female",
+  "favoriteColor": "Blue",
+  "country": "Albania",
+  "birthday": "10/21/1990"
+}, {
+  "firstName": "Brenda",
+  "lastName": "Rose",
+  "gender": "Female",
+  "favoriteColor": "Teal",
+  "country": "Thailand",
+  "birthday": "1/15/2000"
+}, {
+  "firstName": "Henry",
+  "lastName": "Gordon",
+  "gender": "Male",
+  "favoriteColor": "Goldenrod",
+  "country": "Indonesia",
+  "birthday": "7/9/1998"
+}, {
+  "firstName": "Melissa",
+  "lastName": "Powell",
+  "gender": "Female",
+  "favoriteColor": "Teal",
+  "country": "Indonesia",
+  "birthday": "4/7/1995"
+}, {
+  "firstName": "Ralph",
+  "lastName": "Baker",
+  "gender": "Male",
+  "favoriteColor": "Fuscia",
+  "country": "Indonesia",
+  "birthday": "6/10/1995"
+}, {
+  "firstName": "Patricia",
+  "lastName": "Daniels",
+  "gender": "Female",
+  "favoriteColor": "Puce",
+  "country": "Indonesia",
+  "birthday": "12/11/1990"
+}, {
+  "firstName": "Robin",
+  "lastName": "Lopez",
+  "gender": "Female",
+  "favoriteColor": "Indigo",
+  "country": "Poland",
+  "birthday": "9/20/1989"
+}, {
+  "firstName": "Emily",
+  "lastName": "Chapman",
+  "gender": "Female",
+  "favoriteColor": "Red",
+  "country": "Russia",
+  "birthday": "7/5/2000"
+}, {
+  "firstName": "Joseph",
+  "lastName": "Rice",
+  "gender": "Male",
+  "favoriteColor": "Yellow",
+  "country": "Philippines",
+  "birthday": "6/19/1993"
+}, {
+  "firstName": "Earl",
+  "lastName": "Thomas",
+  "gender": "Male",
+  "favoriteColor": "Fuscia",
+  "country": "Sweden",
+  "birthday": "3/25/1983"
+}, {
+  "firstName": "Steven",
+  "lastName": "Lawrence",
+  "gender": "Male",
+  "favoriteColor": "Maroon",
+  "country": "Brazil",
+  "birthday": "12/10/1999"
+}, {
+  "firstName": "Angela",
+  "lastName": "Green",
+  "gender": "Female",
+  "favoriteColor": "Crimson",
+  "country": "Niger",
+  "birthday": "3/22/1985"
+}, {
+  "firstName": "Debra",
+  "lastName": "Davis",
+  "gender": "Female",
+  "favoriteColor": "Orange",
+  "country": "Ukraine",
+  "birthday": "4/18/1989"
+}, {
+  "firstName": "Justin",
+  "lastName": "Stewart",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "Palestinian Territory",
+  "birthday": "11/2/1991"
+}, {
+  "firstName": "Paul",
+  "lastName": "Alexander",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "China",
+  "birthday": "8/30/1999"
+}, {
+  "firstName": "Michelle",
+  "lastName": "Mendoza",
+  "gender": "Female",
+  "favoriteColor": "Blue",
+  "country": "Portugal",
+  "birthday": "3/14/1981"
+}, {
+  "firstName": "Aaron",
+  "lastName": "Bryant",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "Nicaragua",
+  "birthday": "11/28/1996"
+}, {
+  "firstName": "Kathy",
+  "lastName": "Cunningham",
+  "gender": "Female",
+  "favoriteColor": "Puce",
+  "country": "Indonesia",
+  "birthday": "10/27/1997"
+}, {
+  "firstName": "Frank",
+  "lastName": "Mills",
+  "gender": "Male",
+  "favoriteColor": "Indigo",
+  "country": "Iran",
+  "birthday": "8/10/1998"
+}, {
+  "firstName": "Eric",
+  "lastName": "Garza",
+  "gender": "Male",
+  "favoriteColor": "Khaki",
+  "country": "New Zealand",
+  "birthday": "9/3/2000"
+}, {
+  "firstName": "Christopher",
+  "lastName": "Banks",
+  "gender": "Male",
+  "favoriteColor": "Aquamarine",
+  "country": "Sweden",
+  "birthday": "4/10/1999"
+}, {
+  "firstName": "Louise",
+  "lastName": "Medina",
+  "gender": "Female",
+  "favoriteColor": "Fuscia",
+  "country": "Philippines",
+  "birthday": "6/3/1991"
+}, {
+  "firstName": "Albert",
+  "lastName": "Taylor",
+  "gender": "Male",
+  "favoriteColor": "Teal",
+  "country": "Indonesia",
+  "birthday": "9/22/1995"
+}, {
+  "firstName": "Diane",
+  "lastName": "Ford",
+  "gender": "Female",
+  "favoriteColor": "Purple",
+  "country": "Russia",
+  "birthday": "12/31/1996"
+}, {
+  "firstName": "Jimmy",
+  "lastName": "Riley",
+  "gender": "Male",
+  "favoriteColor": "Turquoise",
+  "country": "Brazil",
+  "birthday": "5/31/1989"
+}, {
+  "firstName": "Carolyn",
+  "lastName": "Porter",
+  "gender": "Female",
+  "favoriteColor": "Goldenrod",
+  "country": "Philippines",
+  "birthday": "12/12/1983"
+}, {
+  "firstName": "Evelyn",
+  "lastName": "Collins",
+  "gender": "Female",
+  "favoriteColor": "Turquoise",
+  "country": "Ukraine",
+  "birthday": "1/13/2000"
+}, {
+  "firstName": "Stephen",
+  "lastName": "Franklin",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "Brazil",
+  "birthday": "8/1/1997"
+}, {
+  "firstName": "William",
+  "lastName": "Fox",
+  "gender": "Male",
+  "favoriteColor": "Puce",
+  "country": "Indonesia",
+  "birthday": "8/4/1997"
+}, {
+  "firstName": "Raymond",
+  "lastName": "Dean",
+  "gender": "Male",
+  "favoriteColor": "Goldenrod",
+  "country": "Thailand",
+  "birthday": "11/24/1985"
+}, {
+  "firstName": "Carolyn",
+  "lastName": "Russell",
+  "gender": "Female",
+  "favoriteColor": "Yellow",
+  "country": "Philippines",
+  "birthday": "11/18/1983"
+}, {
+  "firstName": "Fred",
+  "lastName": "Spencer",
+  "gender": "Male",
+  "favoriteColor": "Goldenrod",
+  "country": "China",
+  "birthday": "5/10/1987"
+}, {
+  "firstName": "Christopher",
+  "lastName": "Collins",
+  "gender": "Male",
+  "favoriteColor": "Turquoise",
+  "country": "China",
+  "birthday": "3/23/1993"
+}, {
+  "firstName": "Wayne",
+  "lastName": "Lee",
+  "gender": "Male",
+  "favoriteColor": "Green",
+  "country": "Russia",
+  "birthday": "7/21/1999"
+}, {
+  "firstName": "Doris",
+  "lastName": "Hill",
+  "gender": "Female",
+  "favoriteColor": "Aquamarine",
+  "country": "Greece",
+  "birthday": "2/5/1985"
+}, {
+  "firstName": "Evelyn",
+  "lastName": "Cruz",
+  "gender": "Female",
+  "favoriteColor": "Orange",
+  "country": "France",
+  "birthday": "2/8/1985"
+}, {
+  "firstName": "Harold",
+  "lastName": "Fisher",
+  "gender": "Male",
+  "favoriteColor": "Maroon",
+  "country": "Indonesia",
+  "birthday": "1/10/1981"
+}, {
+  "firstName": "Walter",
+  "lastName": "Price",
+  "gender": "Male",
+  "favoriteColor": "Crimson",
+  "country": "Mexico",
+  "birthday": "3/14/1982"
+}, {
+  "firstName": "Nicole",
+  "lastName": "Simmons",
+  "gender": "Female",
+  "favoriteColor": "Pink",
+  "country": "France",
+  "birthday": "7/24/1991"
+}];
